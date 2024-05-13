ce2a78cf34e40d51e9441a15c6bc714dff46d86e
AbdulrahmanAwad <Abdulrahmanawad7861991@gmail.com>
2024-05-13 11:05:04 +0300

    GenericRepository

--- Examination.DAL/Repositories/IGenericRepository.cs
+++ Examination.DAL/Repositories/IGenericRepository.cs
@@ -13,6 +13,17 @@ namespace Examination.DAL.Repositories
             Expression<Func<T, bool>> filter = null!,
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null!,
             string IncludeProperties = "");
+
+        T? GetById(object id);
+
+        Task<T?> GetByIdAsync(object id);
+        void Add(T entity);
+        Task<T> AddAsync(T entity);
+        void DeleteById(object id);
+        void Delete(T entityToDelete);
+        void Update(T entityToUpdate);
+        Task<T> UpdateAsync(T entityToUpdate);
+        Task<T?> DeleteAsync(T entityToDelete);
         
     }
 }
